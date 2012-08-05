f9f3adadde5014bd194533030e8d17ff68b89cff
Nick Osborn <nick@Nicks-MacBook-Air.local>
2012-08-05 15:14:38 +0100

    More Vim plug-ins.

--- update.sh
+++ update.sh
@@ -25,53 +25,11 @@ _fetch local/bin/colordiff \
 # vim
 _fetch vim/colors/molokai.vim \
 	https://raw.github.com/tomasr/molokai/master/colors/molokai.vim
-
-# Sublime Text 2 - Themes - IR_black
-#_fetch 'Sublime Text 2/Packages/Color Scheme - Default/IR_Black.tmTheme' \
-#	http://sublime-text-community-packages.googlecode.com/svn/trunk/Themes/IR_Black.tmTheme
-
-# vim - fugitive
-#_fetch vim/fugitive.zip \
-#	'http://www.vim.org/scripts/download_script.php?src_id=15542'
-
-# vim - haproxy
-#_fetch vim/syntax/haproxy.vim \
-#	'http://www.vim.org/scripts/download_script.php?src_id=6924'
-
-# vim - interfaces
-#_fetch vim/syntax/interfaces.vim \
-#	'http://www.vim.org/scripts/download_script.php?src_id=4693'
-
-# vim - ir_black
-#_fetch vim/colors/ir_black.vim \
-#	'http://blog.toddwerth.com/entry_files/8/ir_black.vim'
-
-# vim - json
-#_fetch vim/syntax/json.vim \
-#	'http://www.vim.org/scripts/download_script.php?src_id=10853'
-
-# vim - markdown
-#for f in ftdetect/markdown.vim syntax/markdown.vim; do
-#	_fetch vim/$f https://raw.github.com/hallison/vim-markdown/master/$f
-#done
-
-# vim - openssl
-#_fetch vim/ftplugin/openssl.vim \
-#	'http://www.vim.org/scripts/download_script.php?src_id=8564'
-
-## vim - pathogen
-#_fetch vim/autoload/pathogen.vim \
-#	https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-# vim - puppet
-#for f in ftplugin/puppet.vim indent/puppet.vim syntax/puppet.vim; do
-#	_fetch vim/$f https://raw.github.com/puppetlabs/puppet-syntax-vim/master/$f
-#done
-
-# vim - solarized
-#_fetch vim/colors/solarized.vim \
-#	https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
-
-# vim - syntastic
-#_fetch vim/syntastic.zip \
-#  'http://www.vim.org/scripts/download_script.php?src_id=17114'
+_fetch vim/plugin/openssl.vim \
+	'http://www.vim.org/scripts/download_script.php?src_id=8564'
+_fetch vim/syntax/haproxy.vim \
+	'http://www.vim.org/scripts/download_script.php?src_id=6924'
+_fetch vim/syntax/interfaces.vim \
+	'http://www.vim.org/scripts/download_script.php?src_id=4693'
+_fetch vim/syntax/json.vim \
+	'http://www.vim.org/scripts/download_script.php?src_id=10853'
