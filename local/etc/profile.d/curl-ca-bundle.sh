a86d8baf825de0a316fc49ca2ed2d1157b8f174c
Nick Osborn <nickosborn@redzebraconsulting.com>
2013-06-16 23:05:16 +0100

    Use updated ca-bundle if available.

---
+++ local/etc/profile.d/curl-ca-bundle.sh
@@ -0,0 +1,5 @@
+if [ -z "${SSL_CERT_FILE}" ]; then
+	if [ -e /usr/local/opt/curl-ca-bundle/share/ca-bundle.crt ]; then
+		export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
+	fi
+fi
