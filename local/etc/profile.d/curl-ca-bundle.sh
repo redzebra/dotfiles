2b8d76acdb5b28f0711d0316ef5f673ca5e47427
Nick Osborn <nickosborn@redzebraconsulting.com>
2013-06-20 19:31:42 +0100

    Obsessive-compulsive whitespace cleanup.

--- local/etc/profile.d/curl-ca-bundle.sh
+++ local/etc/profile.d/curl-ca-bundle.sh
@@ -1,5 +1,5 @@
 if [ -z "${SSL_CERT_FILE}" ]; then
-	if [ -e /usr/local/opt/curl-ca-bundle/share/ca-bundle.crt ]; then
-		export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
-	fi
+  if [ -e /usr/local/opt/curl-ca-bundle/share/ca-bundle.crt ]; then
+    export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
+  fi
 fi
